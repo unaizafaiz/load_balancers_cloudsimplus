f1f5985c030c9dfd4043580758fbcb6c9a89c529
Unaiza Faiz <unaizafaiz@dhcp-vlan876-9-217-126.vlan861.client.wireless.uic.edu>
2019-05-11 16:07:07 -0500

    Updated comments

--- src/main/java/org/cloudbus/cloudsim/utilzationmodels/UtilizationModelHalf.java
+++ src/main/java/org/cloudbus/cloudsim/utilzationmodels/UtilizationModelHalf.java
@@ -5,17 +5,15 @@ import org.cloudbus.cloudsim.utilizationmodels.UtilizationModel;
 import org.cloudbus.cloudsim.utilizationmodels.UtilizationModelAbstract;
 /**
  * A {@link UtilizationModel} that according to which, a Cloudlet always utilizes
- * a given allocated resource from its Vm at 100%, all the time.
+ * a given allocated resource from its Vm at 50%, all the time.
  *
- * @author Anton Beloglazov
- * @since CloudSim Toolkit 2.0
  */
 public class UtilizationModelHalf extends UtilizationModelAbstract {
     /**
      * Gets the utilization percentage (in scale from [0 to 1]) of resource at a given simulation time.
      *
      * @param time the time to get the resource usage.
-     * @return Always return 1 (100% of utilization), independent of the time.
+     * @return Always return 5 (0.5% of utilization), independent of the time.
      */
     @Override
     public double getUtilization(double time) {
@@ -25,7 +23,7 @@ public class UtilizationModelHalf extends UtilizationModelAbstract {
     /**
      * Gets the utilization percentage (in scale from [0 to 1]) of resource at the current simulation time.
      *
-     * @return Always return 1 (100% of utilization), independent of the time.
+     * @return Always return 0.5 (50% of utilization), independent of the time.
      */
     @Override
     public double getUtilization() {
